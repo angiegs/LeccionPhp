ae710db2ee6ce9d2e304124ae6ff9cd58e45411b
Angelica Garces <abgs15@gmail.com>
2017-08-25 13:58:37 -0500

    ELiminar actual

--- leccion/eliminar.php
+++ leccion/eliminar.php
@@ -6,11 +6,17 @@
 	<div id="main">
 	<?php
 	$id=$_GET["id"];
+	$nombre=$_GET["nombre"];
+
 	include_once("DemoCollector.php");
 	$DemoCollector= new DemoCollector();
 	$DemoCollector->deleteDemo($id);
+		$DemoCollector->deleteDemo($nombre);
+
 
 	echo "id  :".$iddemo." Se HA ELIMINADO :</br>";
+		echo "Nombre  :".$nombre." Se HA ELIMINADO :</br>";
+
 	?>
 	<div><a href="index.php">Volver al Inicio</a></div>
 
