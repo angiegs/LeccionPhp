e359b44acec8a996531e6687ba95ad2e23d1263a
Angelica Garces <abgs15@gmail.com>
2017-08-25 13:59:43 -0500

    Guardar demo  actual

--- leccion/guardarDemo.php
+++ leccion/guardarDemo.php
@@ -11,12 +11,20 @@
 	<div id="main">
 	<?php
 	$valor=$_POST["nombre"];
+	$tipon=$_POST["tipo"];
+	$fechan=$_POST["fecha"];
+
+
+
 
 	//$valor="Manuel";
 	echo 'Hola ' . htmlspecialchars($valor) . '!';
 	include_once("DemoCollector.php");
 	$DemoCollectorObj = new DemoCollector();
 	$DemoCollectorObj->createDemo($valor);
+	$DemoCollectorObj->createDemo($tipon);
+	$DemoCollectorObj->createDemo($fechan);
+
 	echo "Valor agregado </br>";
 	?>
 	<div><a href="index.php">Volver al inicio</a></div>
